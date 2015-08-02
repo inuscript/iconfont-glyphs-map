6f661b77755bdb0292a1e0e4383c8fc919eb4c45
inuscript <inuscript@gmail.com>
2015-08-02 15:58:46 +0900

    rename

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 var quote = require("quote")
 var empty = function(s){ return s }
 
-module.exports = function(glyphs, withQuote, withBackslash){
+var glyphMap = function(glyphs, withQuote, withBackslash){
   var bs = (withBackslash === true) ? "\\" : ""
-  var fn = (withQuote === true) ? quote : empty
+  var quoteFn = (withQuote === true) ? quote : empty
 
   return glyphs.reduce(function(obj, glyph){
     glyphs.forEach(function(glyph){
       // var code = "\\" + glyph.unicode[0].charCodeAt(0).toString(16).toUpperCase()
       var code = glyph.unicode[0].charCodeAt(0).toString(16).toUpperCase()
-      obj[glyph.name] = fn(bs + code)
+      obj[glyph.name] = quoteFn(bs + code)
     })
     return obj
   }, {})
 }
 
+module.exports = glyphMap
